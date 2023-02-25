157e81af6df8835e85ad05089d54439e5f84e979
crgimenes <crg@crg.eti.br>
2023-02-25 06:20:01 -0300

    use filepath visitor

--- cmd/monitor/main.go
+++ cmd/monitor/main.go
@@ -1,6 +1,15 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
 	"crg.eti.br/go/config"
 	_ "crg.eti.br/go/config/ini"
 )
@@ -21,6 +30,63 @@ func load() (Config, error) {
 	return cfg, nil
 }
 
+type ProcessInfo struct {
+	PID  int
+	Name string
+	CPU  float64
+}
+
+func fileExists(path string) (ret bool) {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return
+		}
+
+		log.Println(err)
+		return
+	}
+
+	ret = true
+	return
+}
+
+func visit(path string, f os.FileInfo, perr error) error {
+	if perr != nil {
+		return perr
+	}
+
+	if !f.IsDir() {
+		return nil
+	}
+
+	/*
+		pathAbs, err := filepath.Abs(path)
+		if err != nil {
+			return err
+		}
+	*/
+	fmt.Println(path)
+	return nil
+}
+
+func readProcesses() ([]ProcessInfo, error) {
+	var processes []ProcessInfo
+	/*
+		files, err := ioutil.ReadDir("/proc")
+		if err != nil {
+			return processes, err
+		}
+	*/
+
+	err := filepath.Walk("/proc", visit)
+	if err == nil || errors.Is(err, io.EOF) {
+		return
+	}
+
+	return processes, nil
+}
+
 func main() {
 	cfg, err := load()
 	if err != nil {
@@ -28,4 +94,32 @@ func main() {
 	}
 
 	println(cfg.MaxProcess)
+
+	loadavg, err := os.ReadFile("/proc/loadavg")
+	if err != nil {
+		panic(err)
+	}
+
+	load := strings.Split(string(loadavg), " ")
+
+	loadLast1, err := strconv.ParseFloat(load[0], 64)
+	if err != nil {
+		panic(err)
+	}
+
+	loadLast5, err := strconv.ParseFloat(load[1], 64)
+	if err != nil {
+		panic(err)
+	}
+
+	loadLast15, err := strconv.ParseFloat(load[2], 64)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Load average: %.2f %.2f %.2f\n", loadLast1, loadLast5, loadLast15)
+
+	readProcesses()
 }
+
+// /proc/stat
