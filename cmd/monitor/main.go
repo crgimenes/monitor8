76b8af38eef2b33d02c8045dac7ce2496def895c
crgimenes <crg@crg.eti.br>
2023-02-25 16:54:51 -0300

    test cpu usage

--- cmd/monitor/main.go
+++ cmd/monitor/main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
@@ -18,6 +19,10 @@ import (
 type Config struct {
 	MaxProcess int `json:"max_process" ini:"max_process" cfg:"max_process" cfgDefault:"10" cfgHelper:"Maximum number of processes to collect"`
 }
+
+var (
+	uptimeSeconds float64
+)
 
 func load() (Config, error) {
 	var cfg = Config{}
@@ -52,29 +57,75 @@ func fileExists(path string) (ret bool) {
 	return
 }
 
-func visit(path string, f os.FileInfo, perr error) error {
-	if perr != nil {
-		return perr
+func visit(path string, f fs.DirEntry, err error) error {
+	if err != nil {
+		return err
 	}
 
-	if f.IsDir() {
+	if !f.IsDir() {
 		return nil
 	}
 
+	if !fileExists(path + "/stat") {
+		return nil
+	}
+
+	pid, err := strconv.Atoi(f.Name())
+	if err != nil {
+		return nil
+	}
+	if pid == 0 {
+		return nil
+	}
+
+	stat, err := os.ReadFile(path + "/stat")
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	fields := strings.Fields(string(stat))
 	/*
-		pathAbs, err := filepath.Abs(path)
-		if err != nil {
-			return err
+		for i, field := range fields {
+			fmt.Printf("%d %q\n", i, field)
 		}
 	*/
-	fmt.Println(path)
+	/*
+	   #14 utime - CPU time spent in user code, measured in clock ticks
+	   #15 stime - CPU time spent in kernel code, measured in clock ticks
+	   #16 cutime - Waited-for children's CPU time spent in user code (in clock ticks)
+	   #17 cstime - Waited-for children's CPU time spent in kernel code (in clock ticks)
+	   #22 starttime - Time when the process started, measured in clock ticks
+	*/
+
+	utime, _ := strconv.Atoi(fields[13])
+	stime, _ := strconv.Atoi(fields[14])
+	cutime, _ := strconv.Atoi(fields[15])
+	cstime, _ := strconv.Atoi(fields[16])
+	starttime, _ := strconv.ParseFloat(fields[21], 64)
+
+	total_time := utime + stime + cutime + cstime
+
+	const _SYSTEM_CLK_TCK = 100
+
+	Hertz := float64(_SYSTEM_CLK_TCK)
+
+	seconds := uptimeSeconds - (starttime / Hertz)
+
+	if seconds == 0 {
+		seconds = 1
+	}
+
+	cpu_usage := 100 * ((float64(total_time) / Hertz) / seconds)
+
+	fmt.Printf("PID: %d path %q cpu_usage %v\n", pid, path, cpu_usage)
 	return nil
 }
 
 func readProcesses() ([]ProcessInfo, error) {
 	var processes []ProcessInfo
 
-	err := filepath.Walk("/proc", visit)
+	err := filepath.WalkDir("/proc", visit)
 	if err == nil || errors.Is(err, io.EOF) {
 		return processes, nil
 	}
@@ -83,6 +134,10 @@ func readProcesses() ([]ProcessInfo, error) {
 }
 
 func main() {
+
+	// set uid to root
+	//syscall.Seteuid(0)
+
 	if syscall.Getuid() != 0 {
 		fmt.Printf("you must be root to run %q\n", os.Args[0])
 		os.Exit(1)
@@ -118,6 +173,22 @@ func main() {
 	}
 
 	fmt.Printf("Load average: %.2f %.2f %.2f\n", loadLast1, loadLast5, loadLast15)
+
+	// le o arquivo /proc/uptime e pega o primerio campo, convertendo para int
+	uptime, err := os.ReadFile("/proc/uptime")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("uptime", string(uptime))
+	uptimeFields := strings.Fields(string(uptime))
+	fmt.Println("uptimeFields", uptimeFields)
+	uptimeSeconds, err = strconv.ParseFloat(uptimeFields[0], 64)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("Uptime: %v seconds\n", uptimeSeconds)
 
 	readProcesses()
 }
